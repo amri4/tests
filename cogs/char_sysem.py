199b134634f99438a592d99de217e88dd054ffd4
amri4 <amri.supreme123@gmail.com>
2026-05-17 08:41:16 +0300

    Add Characters cog for character claiming feature

---
+++ cogs/char_sysem.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+from utils import characters
+
+class Characters(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def claim(self, ctx):
+        claimed = characters.get_character(ctx.author.id)
+        if claimed:
+            await ctx.reply(f"⚠️ You already claimed **{claimed}**")
+            return
+
+        if character not in characters.character:
+            await ctx.reply("❌️ Character doesn’t exist")
+            return
+
+        full_name = characters.characters[character]
+
+        if character.is_claimed(full_name):
+            await ctx.reply("💀 This character is already claimed")
+            return
+
+        characters.give_character(ctx.author.id, full_name)
+
+        await ctx.send(f"✅️ character assigned as **{full_name}**")
+
+async def setup(bot):
+    await bot.add_cog(Characters(bot))
