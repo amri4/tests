901a88ca50b12d9e2ec78b4c944e7d78f0a2c9b3
amri4 <amri.supreme123@gmail.com>
2026-06-13 07:26:54 +0300

    Add mycord import to main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import mycord
 
 intents = discord.Intents.default()
 intents.message_content = True  
